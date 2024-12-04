93d914a551166483c638ebd9adc9cb2b857f715e
vahij <vahijhija@gmail.com>
2024-12-04 14:59:41 +0000

    Changing inception

---
+++ 04122024_145941_inception_qx2j1.sh
@@ -0,0 +1,1 @@
+The company "Sony" was originally called "Totsuken." They felt the name "Sony" would be easier to pronounce. The name was invented by a cross between the name "sonus" and "sonny." The name sound and sonic are derived. Sonny was used to represent a young man or boy, which would show a energetic young company
